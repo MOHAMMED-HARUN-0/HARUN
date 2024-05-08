873dfedcf4fe2a7864b0414c8370f531ed9a2502
MOHAMMED HARUN <165628635+MOHAMMED-HARUN-0@users.noreply.github.com>
2024-05-08 17:38:18 +0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,371 @@
+import os,sys
+os.system("xdg-open https://t.me/@arkawsar0") 
+os.system('pkg install espeak')
+#--------------------loading system01-----------------#
+import rich,time
+
+from rich.progress import track
+def lood(message):
+ for a in track (range(250), description = message):
+  time.sleep(0.02)
+  
+#lood("যা মন চাই এইখানে লেখেন") 
+
+#--------------------------loading system-----------------#
+import time
+import sys
+
+def customloadinganimationtwo():
+    sys.stdout.write(       f"{g}[🔰]𝙿𝙻𝙴𝙰𝚂𝙴 𝚆𝙰𝙸𝚃 𝙵𝙾𝚁 𝙻𝙾𝙰𝙳𝙸𝙽𝙶... \n")
+    animation = ["[\x1b[1;91m■\x1b[0m□□□□□□□□□]","[\x1b[1;92m■■\x1b[0m□□□□□□□□]", "[\x1b[1;93m■■■\x1b[0m□□□□□□□]", "[\x1b[1;94m■■■■\x1b[0m□□□□□□]", "[\x1b[1;95m■■■■■\x1b[0m□□□□□]", "[\x1b[1;96m■■■■■■\x1b[0m□□□□]", "[\x1b[1;97m■■■■■■■\x1b[0m□□□]", "[\x1b[1;98m■■■■■■■■\x1b[0m□□]", "[\x1b[1;99m■■■■■■■■■\x1b[0m□]", "[\x1b[1;910m■■■■■■■■■■\x1b[0m]"]
+    
+    for i in range(50):
+        sys.stdout.write("\r" + animation[i % len(animation)])
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+    sys.stdout.write("\rLoading complete!     \n")
+    
+#customloadinganimationtwo()
+
+#-----------𝗳𝗼𝗿𝗺𝗮𝘁  𝗰𝗼𝗹𝗼𝘂𝗿 𝗰𝗼𝗱𝗲------------#
+a="\033[1;34m"#----------𝗯𝗹𝘂𝗲
+b="\033[1;30m"#--------𝗯𝗹𝗮𝗰𝗸
+c="\033[1;36m"#----------𝗰𝘆𝗮𝗻
+g="\033[1;32m"#----------𝗴𝗿𝗲𝗲𝗻
+p="\033[1;35m"#----------𝗽𝘂𝗿𝗽𝗹𝗲
+r="\033[1;31m"#----------𝗿𝗲𝗱
+y="\033[1;33m"#----------𝘆𝗲𝗹𝗹𝗼𝘄
+e="\033[1;37m"#----------𝘄𝗵𝗶𝘁𝗲 {𝗲𝗻𝗱} 
+logo=(f'''
+{g}➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤
+{a}➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤
+
+{y}   __    __     __    _____             🇭      
+{p}   |  |  |  |   / /   | ____|           🇦     
+{c}   |  |__|  |  / /_   | |__             🇷           
+{r}   |   __   | | '_ \  |___ \            🇺               
+{a}   |  |  |  | | (_) |  ___) |           🇳                   
+{g}   |__|  |__|  \___/  |____/                                           
+
+    {p}「𝐓𝐄𝐑𝐌𝐔𝐗 𝐁𝐀𝐒𝐈𝐂 & 𝐅𝐔𝐋𝐋 𝐒𝐄𝐓𝐔𝐏」
+
+{a}➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤
+{g}➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤ ➤
+''')
+menu=(f'''
+     {c}⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆
+     {y}⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆⭆
+     {a}⟰⟰    [𝟬𝟭]「𝐓𝐞𝐫𝐦𝐮𝐱 𝐁𝐚𝐬𝐢𝐜 𝐒𝐞𝐭𝐮𝐩」 ⟱⟱
+     {g}⟰⟰    [𝟬𝟮]「𝐓𝐞𝐫𝐦𝐮𝐱 𝐅𝐮𝐥𝐥 𝐒𝐞𝐭𝐮𝐩」  ⟱⟱
+     {c}⟰⟰    [𝟬𝟯]「𝐁𝐚𝐬𝐢𝐜 𝐒𝐞𝐭𝐮𝐩 𝐋𝐢𝐬𝐭」   ⟱⟱
+     {a}⟰⟰    [𝟬𝟰]「𝐅𝐮𝐥𝐥 𝐒𝐞𝐭𝐮𝐩 𝐋𝐢𝐬𝐭」    ⟱⟱
+     {p}⟰⟰    [𝟬𝟱]「𝗢𝘄𝗻𝗲𝗿 𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺 𝗜𝗗」  ⟱⟱
+    {r} ⟰⟰    [𝟬𝟲]「𝗢𝘄𝗻𝗲𝗿 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 𝗜𝗗」  ⟱⟱
+     {c}⟰⟰    [𝟬𝟳]「𝐄𝐱𝐢𝐭」               ⟱⟱
+     {y}⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅
+     {c}⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅⭅
+''')
+
+def clear():
+	os.system('clear')
+	print(logo)
+
+def akash():	
+	print(logo)
+	print(menu)
+	os.system("xdg-open https://www.facebook.com/mohammed.harun.0") 	
+	ab=input("\033[1;32m[<>]𝐂𝐡𝐨𝐢𝐜𝐞 𝐘𝐨𝐮𝐫 𝐎𝐩𝐭𝐢𝐨𝐧: ")
+	if ab in ['01','1','a','A']:		
+		os.system('espeak -a300 " wait for loading"')
+		customloadinganimationtwo()
+		os.system('clear')	
+		os.system('espeak -a300 " basic setup starting"')
+		basic()
+		clr()
+		os.system('espeak -a300 "basic setup compleated"')
+		input(f'{P}[🔰]𝐏𝐫𝐞𝐬𝐬 𝐄𝐧𝐭𝐞𝐫 𝐅𝐨𝐫 𝐁𝐚𝐜𝐤 𝐓𝐨 𝐌𝐚𝐢𝐧 𝐌𝐞𝐧𝐮')
+		akash()
+	if ab in['02','2','b','B']:
+		lood(f"{g}[⏳]𝐋𝐎𝐀𝐃𝐈𝐍𝐆...]    ") 		 
+		os.system('clear')
+		os.system('espeak -a300 "full setup starting"')
+		full()
+		clr()
+		os.system('espeak -a300 "full setup compleated"')
+		input(f'{y}[🔰]𝐏𝐫𝐞𝐬𝐬 𝐄𝐧𝐭𝐞𝐫 𝐅𝐨𝐫 𝐁𝐚𝐜𝐤 𝐓𝐨 𝐌𝐚𝐢𝐧 𝐌𝐞𝐧𝐮{e}')
+		akash()
+	if ab in ['03','3','c','C']:
+		basic_list()
+		input(f'{y}   [🔰]𝐏𝐫𝐞𝐬𝐬 𝐄𝐧𝐭𝐞𝐫 𝐅𝐨𝐫 𝐁𝐚𝐜𝐤 𝐓𝐨 𝐌𝐚𝐢𝐧 𝐌𝐞𝐧𝐮{e}')
+		os.system('clear')
+		akash()
+	if ab in ['04','4','d','D']:
+		full_list()
+		input(f'{y}   [🔰]𝐏𝐫𝐞𝐬𝐬 𝐄𝐧𝐭𝐞𝐫 𝐅𝐨𝐫 𝐁𝐚𝐜𝐤 𝐓𝐨 𝐌𝐚𝐢𝐧 𝐌𝐞𝐧𝐮{e}')
+		os.system('clear')
+		akash()
+	if ab in['05','5','e','E']:
+		telegram()
+		akash()
+	if ab in ['06','6','f','F']:
+		facebook()
+		akash()
+	if ab in['07','7','g','G']:
+		sys.exit()
+			
+def basic():
+	clear()
+	os.system('''
+	pkg update
+pkg upgrade
+pkg install python
+pkg install python2
+pkg install python3
+pkg install wget
+pkg install git
+pkg install bash
+pkg install python-pip
+pip2 install wget
+pip install bs4
+pip2 install bs4
+pip install requests
+pip2 install requests
+pip install mechanize
+pip2 install mechanize
+pip install php
+pip2 install php
+
+''')
+			
+def full():
+	clear()
+	os.system('''
+	pkg update
+pkg upgrade
+pkg install python
+pkg install python2
+pkg install python3
+pkg install python-pip
+pkg install wget
+pkg install fish
+pkg install ruby
+pkg install help
+pkg install git
+pkg install dnsutils 
+pkg install php
+pkg install perl
+pkg install lua
+pkg install parallel
+pkg install nmap
+pkg install bash
+pkg install clang
+pkg install nano
+pkg install w3m
+pkg install hydra
+pkg install figlet
+pkg install cowsay
+pkg install curl
+pkg install tar
+pkg install zip
+pkg install unzip
+pkg install net-tools
+pkg install tor -y
+pkg install sudo -y
+pkg install wireshark
+pkg install wgetrc
+pkg install wcalc
+pkg install openssl
+pkg install openssl-tool
+pkg install bmon
+pkg install vpn
+pkg install unrar
+pkg install toilet
+pkg install proot
+pkg install net-tools
+pkg install vim
+pkg install vim-python
+pkg install ired
+pkg install goaccess
+pkg install golang
+pkg install tar
+pkg install kibi
+pkg install tsu
+pkg install tmux
+pkg install mtools
+pkg install file
+pkg install vis
+pkg install pass
+pkg install pick
+pkg install chroot
+termux-chroot
+pkg install macchanger
+pkg install ninja
+pkg install elixir
+pkg install swift
+pkg install xmlstarlet
+pkg install fakeroot
+pkg install texinfo
+pkg install netcat
+pkg install wren
+pkg install gatling
+pkg install cvs
+pkg install ffmpeg
+pkg install screen
+pkg install neofetch
+pkg install mariadb
+pkg install picolisp
+pkg install toilet
+pkg install cmatrix
+pkg install dropbear
+pkg install openssh
+pkg install python-pip
+pip2 install wget
+pip install bs4
+pip2 install bs4
+pip install requests
+pip2 install requests
+pip install mechanize
+pip2 install mechanize
+pip install php
+pip2 install php
+
+''')
+
+def basic_list():
+	print('''
+	pkg update
+pkg upgrade
+pkg install python
+pkg install python2
+pkg install python3
+pkg install wget
+pkg install git
+pkg install bash
+pkg install python-pip
+pip2 install wget
+pip install bs4
+pip2 install bs4
+pip install requests
+pip2 install requests
+pip install mechanize
+pip2 install mechanize
+pip install php
+pip2 install php
+
+''')
+	
+def full_list():
+	print('''
+	pkg update
+pkg upgrade
+pkg install python
+pkg install python2
+pkg install python3
+pkg install python-pip
+pkg install wget
+pkg install fish
+pkg install ruby
+pkg install help
+pkg install git
+pkg install dnsutils 
+pkg install php
+pkg install perl
+pkg install lua
+pkg install parallel
+pkg install nmap
+pkg install bash
+pkg install clang
+pkg install nano
+pkg install w3m
+pkg install hydra
+pkg install figlet
+pkg install cowsay
+pkg install curl
+pkg install tar
+pkg install zip
+pkg install unzip
+pkg install net-tools
+pkg install tor -y
+pkg install sudo -y
+pkg install wireshark
+pkg install wgetrc
+pkg install wcalc
+pkg install openssl
+pkg install openssl-tool
+pkg install bmon
+pkg install vpn
+pkg install unrar
+pkg install toilet
+pkg install proot
+pkg install net-tools
+pkg install vim
+pkg install vim-python
+pkg install ired
+pkg install goaccess
+pkg install golang
+pkg install tar
+pkg install kibi
+pkg install tsu
+pkg install tmux
+pkg install mtools
+pkg install file
+pkg install vis
+pkg install pass
+pkg install pick
+pkg install chroot
+termux-chroot
+pkg install macchanger
+pkg install ninja
+pkg install elixir
+pkg install swift
+pkg install xmlstarlet
+pkg install fakeroot
+pkg install texinfo
+pkg install netcat
+pkg install wren
+pkg install gatling
+pkg install cvs
+pkg install ffmpeg
+pkg install screen
+pkg install neofetch
+pkg install mariadb
+pkg install picolisp
+pkg install toilet
+pkg install cmatrix
+pkg install dropbear
+pkg install openssh
+pkg install python-pip
+pip2 install wget
+pip install bs4
+pip2 install bs4
+pip install requests
+pip2 install requests
+pip install mechanize
+pip2 install mechanize
+pip install php
+pip2 install php
+
+''')
+	
+def telegram():
+	clear()
+	os.system("xdg-open https://t.me/@arkawsar0")
+		
+def facebook():
+	clear()
+	os.system("xdg-open https://www.facebook.com/mohammed.harun.0")
+	
+def exit():
+	clear()
+	print(f'{y}𝐓𝐡𝐚𝐧𝐤𝐬 𝐅𝐨𝐫 𝐔𝐬𝐢𝐧𝐠 𝐌𝐲 𝐓𝐨𝐨𝐥...........')
+	
+	
+	
+	
+	
+akash()
+	
+	
+  
+	
